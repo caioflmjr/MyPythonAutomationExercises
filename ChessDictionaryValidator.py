9a7862c6be2ec2eacdb5afab0163e47de433e970
Caio Martins <caioflmjr@gmail.com>
2025-08-01 09:59:02 -0300

    Refactor isValidChessBoard function to remove debug print statements and ensure proper validation logic

--- ChessDictionaryValidator.py
+++ ChessDictionaryValidator.py
@@ -2,9 +2,14 @@
 
 def isValidChessBoard(board):
     for k, v in board.items():
-        if k[0] == 'k':
-            print("yes")
-
-
-dic1 = {'k9': 'as'}
-isValidChessBoard(dic1)
+        if k[1] < 'a' or k[1] > 'h' or k[0] > '8' or k[0] < '1':
+            return False
+        if v[0] not in  ('w', 'b') or v[1:] not in ('pawn', 'knight', 'bishop', 'rook', 'queen', 'king'):
+            return False
+        
+    values = list(board.values())
+    if values.count('wking') != 1 or values.count('bking') != 1:
+        return False
+    
+    else: 
+        return True
