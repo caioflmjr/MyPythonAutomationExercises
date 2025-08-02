e0f2a22404479415c2e50accda5e93f13e7c7040
Caio Martins <caioflmjr@gmail.com>
2025-08-02 16:13:05 -0300

    Refactor printTable function to simplify column width calculation

--- TablePrinter.py
+++ TablePrinter.py
@@ -1,12 +1,16 @@
 def printTable(tableData):
     colWidths = [0] * len(tableData)
     for i, l_interna in enumerate(tableData):
-        maxlen = 0
-        for str in l_interna:
-            if len(str) > maxlen:
-                colWidths[i] = maxlen = len(str)
+        colWidths[i] = max(len(s) for s in l_interna)
 
     for i in range(len(tableData[0])):
         for j in range(len(tableData)):
             print(f"{tableData[j][i].rjust(colWidths[j])}", end=' ')
         print('')
+
+# --- Exemplo de Uso ---
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+printTable(tableData)
